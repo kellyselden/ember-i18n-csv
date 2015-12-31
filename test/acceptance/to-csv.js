4d03c06b9e4b3973c40a7a8754ba2c4d8a07c763
Kelly Selden <kellyselden@gmail.com>
2015-12-31 13:43:43 -0500

    adding coverage

--- test/acceptance/to-csv.js
+++ test/acceptance/to-csv.js
@@ -1,8 +1,10 @@
 var assert = require('assert');
+var test = require('../../lib/test');
 
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
+      test();
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
