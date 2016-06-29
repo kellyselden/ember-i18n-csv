15181a51ef39818eabf90385ef13f0c1a1ec1b19
kellyselden <kellyselden@gmail.com>
2016-06-29 09:14:07 -0700

    simplify dotted rule

--- test/fixtures/locales-dotted/nl-nl/translations.js
+++ test/fixtures/locales-dotted/nl-nl/translations.js
@@ -1,3 +1,10 @@
 export default {
-  "node1.node1-1": "Industrieën"
+  "node1.node1-1": "Industrieën",
+  "node1.node1-2.node1-2-1": "test",
+  "node1.node1-2.node1-2-2": "test",
+  "node2.node2-1": "test",
+  "node2.node2-2.node2-2-1": "test",
+  "node2.node2-2.node2-2-2": "test",
+  "weekday.tuesday": "Dinsdag",
+  "weekday.saturday": "Zaterdag"
 };
